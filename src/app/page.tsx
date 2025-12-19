11d09be50d2ee06e75b0ed68e200ef8a7ad464a7
yujin-fe <hyujin1224@gmail.com>
2025-12-19 20:32:29 +0900

    🐛 Fix: 테스트 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Logo from '@/components/Logo';
 import Text from '@/components/Text';
 
 export default function Page() {
@@ -26,8 +25,6 @@ export default function Page() {
 
       {/* caption */}
       <Text as="span">12px 다람쥐 헌 쳇바퀴에 타고파</Text>
-      <Logo size="w-10" />
-      <Logo size="w-10" />
     </div>
   );
 }
