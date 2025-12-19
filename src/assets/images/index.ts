f920cce26cf6f912ef0f6ca49bfce790ac074f2f
sylee <linggo01@naver.com>
2025-12-19 12:53:53 +0900

    ⚙️ Chore: svgr 컴퍼넌트방식 제거

--- src/assets/images/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ImgLogo } from '@/components/svg/ImgLogo';
-export { default as ImgEmpty } from '@/components/svg/ImgEmpty';
-export { default as ImgProfile } from '@/components/svg/ImgProfile';
-export { default as ImgWarning } from '@/components/svg/ImgWarning';
