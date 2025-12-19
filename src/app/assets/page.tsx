b8832548d88aa0d28d8da969241696de5ecb464a
sylee <linggo01@naver.com>
2025-12-19 10:14:46 +0900

    ⚙️ Chore: 미리보기화면 tailwind 연결

--- src/app/assets/page.tsx
+++ src/app/assets/page.tsx
@@ -1,3 +1,4 @@
+import { CSSProperties } from 'react';
 import {
   IcArt,
   IcArtActive,
@@ -61,8 +62,9 @@ import {
 } from '@/assets/images';
 
 export default function assets() {
-  const boxStyle = {
+  const boxStyle: CSSProperties = {
     display: 'flex',
+    flexWrap: 'wrap',
   };
   return (
     <div>
@@ -73,69 +75,68 @@ export default function assets() {
             display: 'flex',
             backgroundColor: '#000',
           }}>
-          <IcArtActive />
-          <IcBusActive />
-          <IcFoodActive />
-          <IcTourActive />
-          <IcSportActive />
-          <IcWellbeingActive />
+          <IcArtActive className="h-[24px] w-[24px]" />
+          <IcBusActive className="h-[24px] w-[24px]" />
+          <IcFoodActive className="h-[24px] w-[24px]" />
+          <IcTourActive className="h-[24px] w-[24px]" />
+          <IcSportActive className="h-[24px] w-[24px]" />
+          <IcWellbeingActive className="h-[24px] w-[24px]" />
         </span>
-        <IcArt />
-        <IcBus />
-        <IcFood />
-        <IcTour />
-        <IcSport />
-        <IcWellbeing />
-        <IcSetting />
-        <IcSettingActive />
-        <IcCalendar />
-        <IcCalendarActive />
-        <IcUser />
-        <IcUserActive />
-        <IcList />
-        <IcListActive />
-        <IcBell />
-        <IcBack />
-        <IcClose />
-        <IcCalenderNext />
-        <IcCalenderPrev />
-        <IcDropDown />
-        <IcDropUp />
-        <IcPageBack />
-        <IcEdit />
-        <IcImageDelete />
-        <IcImageAdd />
-        <IcImageDisabled />
-        <IcMinus />
-        <IcPlus />
-        <IcMap />
-        <IcMore />
-        <IcPageLeft />
-        <IcPageLeftOff />
-        <IcPageRight />
-        <IcPageRightOff />
-        <IcPasswordHidden />
-        <IcPasswordShow />
-        <IcSearch />
-        <IcStarOff />
-        <IcStartOn />
-        <IcTrash />
-        <IcKakao />
-        <IcYoutube />
-        <IcX />
-        <IcFacebook />
-        <IcInstagram />
+        <IcArt className="h-[24px] w-[24px]" />
+        <IcBus className="h-[24px] w-[24px]" />
+        <IcFood className="h-[24px] w-[24px]" />
+        <IcTour className="h-[24px] w-[24px]" />
+        <IcSport className="h-[24px] w-[24px]" />
+        <IcWellbeing className="h-[24px] w-[24px]" />
+        <IcSetting className="h-[20px] w-[20px]" />
+        <IcSettingActive className="h-[20px] w-[20px]" />
+        <IcCalendar className="h-[20px] w-[20px]" />
+        <IcCalendarActive className="h-[20px] w-[20px]" />
+        <IcUser className="h-[20px] w-[20px]" />
+        <IcUserActive className="h-[20px] w-[20px]" />
+        <IcList className="h-[20px] w-[20px]" />
+        <IcListActive className="h-[20px] w-[20px]" />
+        <IcBell className="h-[24px] w-[24px]" />
+        <IcBack className="h-[24px] w-[24px]" />
+        <IcClose className="h-[24px] w-[24px]" />
+        <IcCalenderNext className="h-[24px] w-[24px]" />
+        <IcCalenderPrev className="h-[24px] w-[24px]" />
+        <IcDropDown className="h-[24px] w-[24px]" />
+        <IcDropUp className="h-[24px] w-[24px]" />
+        <IcPageBack className="h-[39px] w-[39px]" />
+        <IcEdit className="h-[30px] w-[30px]" />
+        <IcImageDelete className="h-[26px] w-[26px]" />
+        <IcImageAdd className="h-[40px] w-[40px]" />
+        <IcImageDisabled className="h-[40px] w-[40px]" />
+        <IcMinus className="h-[24px] w-[24px]" />
+        <IcPlus className="h-[24px] w-[24px]" />
+        <IcMap className="h-[24px] w-[24px]" />
+        <IcMore className="h-[24px] w-[24px]" />
+        <IcPageLeft className="h-[40px] w-[40px]" />
+        <IcPageLeftOff className="h-[40px] w-[40px]" />
+        <IcPageRight className="h-[40px] w-[40px]" />
+        <IcPageRightOff className="h-[40px] w-[40px]" />
+        <IcPasswordHidden className="h-[24px] w-[24px]" />
+        <IcPasswordShow className="h-[24px] w-[24px]" />
+        <IcSearch className="h-[24px] w-[24px]" />
+        <IcStarOff className="h-[24px] w-[24px]" />
+        <IcStartOn className="h-[24px] w-[24px]" />
+        <IcTrash className="h-[22px] w-[22px]" />
+        <IcKakao className="h-[24px] w-[24px]" />
+        <IcYoutube className="h-[24px] w-[24px]" />
+        <IcX className="h-[24px] w-[24px]" />
+        <IcFacebook className="h-[24px] w-[24px]" />
+        <IcInstagram className="h-[24px] w-[24px]" />
       </div>
-
       <br />
       <h1>assets/images</h1>
       <div style={boxStyle}>
-        <ImgLogo />
-        <ImgEmpty />
-        <ImgWarning />
-        <ImgProfile />
-        <ImgMainVisual />
-        <ImgMainBg />
+        <ImgLogo className="h-[144px] w-[144px]" />
+        <ImgEmpty className="h-[122px] w-[122px]" />
+        <ImgWarning className="h-[88px] w-[88px]" />
+        <ImgProfile className="h-[120px] w-[120px]" />
+        <ImgMainVisual className="h-[500px] w-[1120px]" />
+        <ImgMainBg className="h-[2827px] w-[1920px]" />
       </div>
     </div>
   );
