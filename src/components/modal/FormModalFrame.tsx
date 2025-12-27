0f6f6d45f4bfd0bd96fc779f7126ab74bbeb776a
yujin-fe <hyujin1224@gmail.com>
2025-12-27 21:37:16 +0900

    ✨ Feat: 폼 모달  프레임 구현

---
+++ src/components/modal/FormModalFrame.tsx
@@ -0,0 +1,50 @@
+import Button from '../Button';
+
+interface FormModalFrameProps {
+  children: React.ReactNode;
+  submitBtnText: string;
+  disabled?: boolean;
+  onCloseModal: () => void;
+  onSubmit: () => Promise<void>;
+  className?: string;
+}
+
+export default function FormModalFrame({
+  children,
+  submitBtnText,
+  disabled = false,
+  onCloseModal,
+  onSubmit,
+  className,
+}: FormModalFrameProps) {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await onSubmit();
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="bg-background flex h-fit flex-col gap-5 rounded-[30px] px-6 py-6 py-[20px] sm:gap-[30px] sm:px-[30px]">
+      <div className={className}>{children}</div>
+      <div className="flex w-full gap-2.5 sm:mb-5">
+        <Button
+          type="button"
+          size="sm"
+          variant={'secondary'}
+          onClick={onCloseModal}
+          className="w-[85px] sm:h-[54px] sm:w-[107px]">
+          닫기
+        </Button>
+        <Button
+          type="submit"
+          size="sm"
+          variant={'primary'}
+          disabled={disabled}
+          className="bold w-[185px] sm:h-[54px] sm:w-[210px]">
+          {submitBtnText}
+        </Button>
+      </div>
+    </form>
+  );
+}
