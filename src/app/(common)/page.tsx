84fc8b538e340c28cc6cdaad5452008202457c1a
sylee <linggo01@naver.com>
2026-01-08 15:25:53 +0900

    ♻️ Refactor: vercel 오류대응 Suspense로 client 컴퍼넌트 보장처리

--- src/app/(common)/page.tsx
+++ src/app/(common)/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 import LandingPageClient from './components/LandingPageClient';
 
 import { cn } from '@/util/cn';
@@ -5,16 +7,17 @@ import { cn } from '@/util/cn';
 export default function LandingPage() {
   return (
     <div className={cn('overflow-hidden', '-mx-6 px-6', 'md:-mx-8 md:px-8')}>
-      <div className="m-auto max-w-[1200px]">
-        {/* 데이터 패칭 및 로딩/에러 관리는 클라이언트 컴포넌트 */}
-        <LandingPageClient />
+      <div className="m-auto max-w-300">
+        <Suspense fallback={<div>Loading...</div>}>
+          <LandingPageClient />
+        </Suspense>
 
         {/* 배경 */}
         <div
           className={cn(
             'absolute inset-0 z-[-1] bg-[url("/main/img-main-bg.svg")] bg-repeat-x',
-            'bg-[length:1200px_auto] bg-[position:-2%_0]',
-            'md:bg-[length:1920px_auto] md:bg-[position:50%_0]'
+            'bg-size-[1200px_auto] bg-position-[-2%_0]',
+            'md:bg-size-[1920px_auto] md:bg-position-[50%_0]'
           )}></div>
       </div>
     </div>
