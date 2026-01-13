a8c625e76da5fb84074d9f5e1132d0691cc5efa9
yujin-fe <hyujin1224@gmail.com>
2026-01-13 10:31:52 +0900

    🐛 Fix: 반응형 패딩 수정

--- src/app/(common)/(mypage)/myactivities-reservations/page.tsx
+++ src/app/(common)/(mypage)/myactivities-reservations/page.tsx
@@ -21,13 +21,13 @@ export default function Page() {
   const { activities } = myActivitiesData;
   return (
     <div className="mb-4 flex flex-col gap-5 max-md:mx-[-24px] max-md:mt-[10px] md:mb-21 md:gap-6 lg:gap-[30px]">
-      <div className="px-6">
+      <div className="max-md:px-6">
         <h2 className="bold text-[18px] text-gray-950">예약 현황</h2>
         <span className="text-[14px] font-normal text-gray-500">
           내 체험에 에약된 내역들을 한 눈에 확인할 수 있습니다.
         </span>
       </div>
-      <DropDown type="select" className="px-6">
+      <DropDown type="select" className="max-md:px-6">
         <DropDownTrigger placeholder={activities[0].title}></DropDownTrigger>
         <DropDownList>
           {activities.map((activity) => (
