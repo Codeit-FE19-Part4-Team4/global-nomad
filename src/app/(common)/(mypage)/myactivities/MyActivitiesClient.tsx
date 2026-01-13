3cdcc56fe9223e84eb20b006a8a81b332bb0d1a2
sylee <linggo01@naver.com>
2026-01-13 16:07:14 +0900

    ♻️ Refactor: common layout 하단여백

--- src/app/(common)/(mypage)/myactivities/MyActivitiesClient.tsx
+++ src/app/(common)/(mypage)/myactivities/MyActivitiesClient.tsx
@@ -40,7 +40,7 @@ export default function MyActivitiesClient({
           </Button>
 
           {/* 내 체험 목록 */}
-          <div className="mb-10 md:mb-20">
+          <div>
             {data.map((item) => {
               return (
                 <ExperienceManageCard
